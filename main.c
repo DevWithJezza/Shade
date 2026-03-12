2f9cc5f8a132a9cea5f3273ecb1715fe4b5041e2
DevWithJezza <jeremy.murfy@gmail.com>
2026-03-12 15:03:11 +1100

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+int main(void) {
+    if (!glfwInit()) {
+        printf("GLFW failed to initialize\n");
+        return 1;
+    }
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Shade", NULL, NULL);
+
+    glfwTerminate();
+    return 0;
+}
+
